ea9b44232cbbc7ce8b370e261da9742ae4cc730e
lee-ji-an <leeja0407@naver.com>
2023-03-01 14:07:57 +0900

    Chap 5: RequestLine 테스트 코드 추가

---
+++ src/test/java/http/RequestLineTest.java
@@ -0,0 +1,41 @@
+package http;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+public class RequestLineTest {
+
+    @Test
+    public void create_method_get() {
+        RequestLine line = new RequestLine("GET /index.html HTTP/1.1");
+        assertEquals("GET", line.getMethod());
+        assertEquals("/index.html", line.getPath());
+    }
+
+    @Test
+    public void create_method_post() {
+        RequestLine line = new RequestLine("POST /index.html HTTP/1.1");
+        assertEquals("/index.html", line.getPath());
+    }
+
+    @Test
+    public void create_path_and_params_get() {
+        RequestLine line = new RequestLine("GET /user/create?userId=javajigi&password=pass HTTP/1.1");
+        assertEquals("GET", line.getMethod());
+        assertEquals("/user/create", line.getPath());
+        Map<String, String> params = line.getParams();
+        assertEquals(2, params.size());
+    }
+
+    @Test
+    public void create_path_and_params_post() {
+        RequestLine line = new RequestLine("POST /user/create?id=1 HTTP/1.1");
+        assertEquals("POST", line.getMethod());
+        assertEquals("/user/create", line.getPath());
+        Map<String, String> params = line.getParams();
+        assertEquals(1, params.size());
+    }
+}
