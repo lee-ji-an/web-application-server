732f7c7caf07c445ab716619c1620d20168bf7be
lee-ji-an <leeja0407@naver.com>
2023-10-27 18:23:32 +0900

    Remove: 불필요한 클래스 삭제

--- src/main/java/mvc/controller/CreateUserController.java
+++ src/main/java/mvc/controller/CreateUserController.java
@@ -11,7 +11,7 @@ public class CreateUserController implements Controller {
     public String execute(HttpRequest httpRequest, HttpResponse httpResponse) {
         User user = new User(httpRequest.getParameter("userId"), httpRequest.getParameter("password"), httpRequest.getParameter("name"), httpRequest.getParameter("email"));
         DataBase.addUser(user);
-//        httpResponse.sendRedirect("/index.html");
+
         return "redirect:/index.html";
     }
 }
